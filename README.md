8c0915a4a81f9edd578da3b0a6406111f68eb4ee
Markus Becker <mtib.becker@gmail.com>
2023-04-16 20:32:55 +0200

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mailgun-sending-app
 WebApp to send parameterised E-Mails
+
+## Dependencies
+
+- npm (recommend https://github.com/nvm-sh/nvm to get it)
+
+## Setup
+
+- npm install
--- src/MailgunSenderApp.tsx
+++ src/MailgunSenderApp.tsx
@@ -1,29 +1,51 @@
 import * as React from "react";
 import {
-  CssBaseline,
-  ThemeProvider,
-  Container,
-  useTheme,
-  TextField,
-  Typography,
+    CssBaseline,
+    ThemeProvider,
+    Container,
+    TextField,
+    Typography,
+    createTheme,
+    Stack,
+    Divider,
+    Button,
 } from "@mui/material";
 
-const MailgunSenderApp = () => {
-  const theme = useTheme();
-  const [inputValue, setInputValue] = React.useState("");
+const theme = createTheme({ palette: { mode: "dark" } });
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container>
-        <Typography variant="h1">Mailgun App</Typography>
-        <TextField
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-        />
-      </Container>
-    </ThemeProvider>
-  );
+const MailgunSenderApp = () => {
+    const [apiKey, setApiKey] = React.useState("");
+    const [kundenName, setKundenName] = React.useState("");
+
+    // UI Elemente von https://mui.com/
+    // Network mit fetch https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
+    const onButtonClick = React.useCallback(() => {
+        alert("TODO");
+    }, [apiKey, kundenName]);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container>
+                <Stack>
+                    <Typography variant="h2">Mailgun App</Typography>
+                    <Typography variant="body1">API KEY</Typography>
+                    <TextField
+                        value={apiKey}
+                        onChange={(event) => setApiKey(event.target.value)}
+                    />
+                    <Divider />
+                    <Typography variant="body1">Name von Kunde</Typography>
+                    <TextField
+                        value={kundenName}
+                        onChange={(event) => setKundenName(event.target.value)}
+                    />
+                    <Button onClick={onButtonClick} />
+                </Stack>
+            </Container>
+        </ThemeProvider>
+    );
 };
 
 export default MailgunSenderApp;
